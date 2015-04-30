9294e6bcf9ab32624736f01ba3697c495b414e2f
unknown <d.andres.santiago@hotmail.es>
2015-04-30 15:34:20 +0200

    El puñetero proyecto

---
+++ Pruebas_2D/Assets/scenarios/scripts/lightController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Se empleara este script para hacer titilar las luces del escenario.
+//Asimismo, para causar un ambiente mas realista, se hara incrementar
+//y decrementar el radio de proyeccion de las luces con la misma cadencia
+//que la intensidad.
+
+public class lightController : MonoBehaviour {
+
+	private Light the_light;
+	private bool incremento = true;
+
+	public float max_intensity = 8f;
+	public float min_intensity = 2f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		the_light = gameObject.GetComponent<Light>();
+
+		//Si la luz incrementa
+		if (incremento) {
+			the_light.intensity += 0.1f;
+		//Si la luz decrementa
+		} else {
+			the_light.intensity -= 0.1f;
+		}
+
+		//Si se ha llegado a los limites maximos de intensidad de luz y rango, la luz decrementara: esta calculado para que lleguen a la vez
+		if (the_light.intensity >= max_intensity){
+			incremento = false;
+		}
+
+		//Si se ha llegado a los limites minimos de intensidad de luz y rango, la luz incrementara: esta calculado para que lleguen a la vez
+		if (the_light.intensity <= min_intensity) {
+			incremento = true;
+		}
+	
+	}
+}
---
+++ Pruebas_2D/Assets/scripts/robot_controler.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class robot_controler : MonoBehaviour {
+
+    //COMPONENTES
+    //-----------
+
+    //Variables lógicas para llevar cuenta del estado de los controles
+    //del personaje.
+
+    //MOVERSE A LA DERECHA
+    private bool move_right = false;
+
+    //MOVERSE A LA IZQUIERDA
+    private bool move_left = false;
+
+    //SALTAR
+    private bool is_jumping = false;
+    private bool is_jumping_up = true;
+
+    //**********************************************************************
+
+    //Variables de componentes del objeto de juego
+    private Transform trans;
+
+    //**********************************************************************
+
+    //Variables adicionales.
+
+    private float speed = 3f;
+    private float jump_speed = 8f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (!move_left&&!move_right)
+        {
+            GetComponent<Animator>().Play("standing_robot");
+        }
+
+        //Se obtiene la transformada del personaje, por si
+        //hubiera que afectar su posición.
+        trans = GetComponent<Transform>();
+	    
+        //Se comprueba el estado del personaje y se
+        //actualiza sus componentes lógicas asociadas
+        //en consecuencia.
+
+        float axis = Input.GetAxisRaw("Horizontal");
+        if (axis == -1)
+        {
+            move_left = true;
+        }
+        else
+        {
+            if (axis == 1)
+            {
+                move_right = true;
+            }
+            else
+            {
+                move_right = move_left = false;
+            }
+        }
+
+        //Actualización de otros valores
+        is_jumping = Input.GetButtonDown("Jump");
+        is_jumping_up = Input.GetButtonUp("Jump");
+
+
+        //Se actúa sobre la transformada de manera coherente al estado del personaje.
+        if (move_right)
+        {
+            GetComponent<Animator>().Play("running_robot");
+            trans.position += new Vector3(speed * Time.deltaTime, 0.0f, 0.0f);
+        }
+
+        if (move_left)
+        {
+            GetComponent<Animator>().Play("running_robot");
+            trans.position -= new Vector3(speed * Time.deltaTime, 0.0f, 0.0f);
+        }
+
+        if (is_jumping)
+        {
+            GetComponent<Rigidbody2D>().velocity +=  new Vector2(transform.up.x, transform.up.y)*jump_speed;
+            //GetComponent<Rigidbody2D>().AddForce(trans.up*jump_speed, ForceMode2D.Impulse);
+            //trans.Translate(Vector3.up * jump_speed * Time.deltaTime, Space.World);
+        }
+
+        if (is_jumping_up)
+        {
+            GetComponent<Rigidbody2D>().velocity +=  new Vector2(0, 0);
+        }
+	}
+}
